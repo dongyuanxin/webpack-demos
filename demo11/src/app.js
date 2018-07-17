d6441fd63e5a7e267d69e4a66fe1bf2bb34c71b3
dongyuanxin <yuanxin.me@gmail.com>
2018-07-17 14:13:14 +0800

    添加: 字体文件处理

---
+++ demo11/src/app.js
@@ -0,0 +1,4 @@
+import "style-loader/lib/addStyles";
+import "css-loader/lib/css-base";
+
+import "./assets/fonts/iconfont.css";
---
+++ demo11/webpack.config.js
@@ -0,0 +1,56 @@
+/**
+ * 1. 利用url-loader可以处理字体文件
+ * 2. 可以根据大小来决定是否进行base64编码
+ */
+
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let extractTextPlugin = new ExtractTextPlugin({
+  filename: "[name].min.css",
+  allChunks: false
+});
+
+module.exports = {
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    publicPath: __dirname + "/dist/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].chunk.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: {
+            loader: "style-loader"
+          },
+          use: [
+            {
+              loader: "css-loader"
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(eot|woff2?|ttf|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[name]-[hash:5].min.[ext]",
+              limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
+              publicPath: "fonts/",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [extractTextPlugin]
+};
