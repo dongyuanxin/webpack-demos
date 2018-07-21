c3f837279cb515010400fc4496935d6f8bd0430f
dongyuanxin <yuanxin.me@gmail.com>
2018-07-21 17:05:20 +0800

    测试: webpack-dev-server

---
+++ demo15/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: "standard",
+  plugins: ["html"],
+  env: {
+    browser: true,
+    node: true
+  },
+  rules: {}
+};
---
+++ demo15/vendor/sum.js
@@ -0,0 +1,4 @@
+export default function(a, b) {
+  console.log("I am sum.js");
+  return a + b;
+}
