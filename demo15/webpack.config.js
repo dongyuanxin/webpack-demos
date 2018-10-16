0bfd6f4f97015eb46df0bf0bb666b11eab6cd7b7
dongyuanxin <yuanxin.me@gmail.com>
2018-10-16 17:14:49 +0800

    完善: web-dev-server示例代码

--- demo15/webpack.config.js
+++ demo15/webpack.config.js
@@ -2,7 +2,7 @@
  * 1. webpack-dev-server: 配置 devServer 选项
  * 2. SourceMap: 配置 devtool 选项( 不同的loader也应该打开对应的sourcemap选项 )
  * 3. 设置代理: devServer.proxy 进行跨域代理设置
- * 4. 路由 rewrite: devServer.historyApiFallback
+ * 4. 开启 HTML5 History API : devServer.historyApiFallback
  * 5. 模块热更新: HotModuleReplacementPlugin && NamedModulesPlugin, 更多请见 './app.js'
  */
 const webpack = require("webpack");
@@ -20,6 +20,7 @@ module.exports = {
     filename: "[name]-[hash:5].bundle.js",
     chunkFilename: "[name]-[hash:5].chunk.js"
   },
+  mode: "development",
   devtool: "source-map",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
@@ -35,21 +36,10 @@ module.exports = {
           Cookie: ""
         }
       }
+    },
+    historyApiFallback: {
+      rewrites: [{ from: /.*/, to: "/index.html" }]
     }
-    // historyApiFallback: {
-    //   rewrites: [
-    //     // {
-    //     //   from: /^\/([a-zA-Z0-9]+\/?)([a-zA-Z0-9]+)/,
-    //     //   to: function(ctx) {
-    //     //     return "/" + ctx.match[1] + ctx.match[2] + ".html";
-    //     //   }
-    //     // },
-    //     {
-    //       from: /./,
-    //       to: "dist/index.html"
-    //     }
-    //   ]
-    // }
   },
   plugins: [
     new HtmlWebpackPlugin({
